bb52aabdcaff630dbd78636988904f642f131ac8
cerahberangin <101511362+cerahberangin@users.noreply.github.com>
2022-06-29 15:37:30 +0700

    Learned

--- javascript/asynchronous/sequencing-animations/start/main.js
+++ javascript/asynchronous/sequencing-animations/start/main.js
@@ -12,3 +12,32 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+// // Promise callback implementation
+// const alice1Promise = alice1.animate(aliceTumbling, aliceTiming).finished;
+// alice1Promise.then(() => {
+//   const alice2Promise = alice2.animate(aliceTumbling, aliceTiming).finished;
+//   alice2Promise.then(() => {
+//     const alice3Promise = alice3.animate(aliceTumbling, aliceTiming).finished;
+//   })
+// })
+
+
+
+// Promise chain implementation
+// alice1.animate(aliceTumbling, aliceTiming).finished
+//   .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+//   .then(() => alice3.animate(aliceTumbling, aliceTiming).finished);
+
+// async and await implementation
+async function animateAlice() {
+  try {
+    await alice1.animate(aliceTumbling, aliceTiming).finished;
+    await alice2.animate(aliceTumbling, aliceTiming).finished;
+    await alice3.animate(aliceTumbling, aliceTiming).finished;
+  } catch (error) {
+    console.error(`Could not animate alice: ${error}`);
+  }
+}
+
+animateAlice();
